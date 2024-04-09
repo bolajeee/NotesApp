3e05b05e3d64fa5c06125ba435f85ff150b8507a
ibrahim O. Ibrahim <ibrahimomobolaji1999@gmail.com>
2024-04-09 12:38:47 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import { nanoid } from "nanoid";
 export default function App() {
   const prevSavedNote = JSON.parse(localStorage.getItem("savedNote"));
 
-  const [notes, setNotes] = React.useState(prevSavedNote || []);
+  const [notes, setNotes] = React.useState(Array.isArray(prevSavedNote)? prevSavedNote : []);
   const [currentNoteId, setCurrentNoteId] = React.useState(
     (notes[0] && notes[0].id) || ""
   );
@@ -29,32 +29,32 @@ export default function App() {
 
   function updateNote(text, currentNote) {
     // Find the note with the same id as the current note
-    const currentNotee = notes.find((note) => note.id === currentNote.id);
+    // const currentNotee = notes.find((note) => note.id === currentNote.id);
 
     // Create a new note object with the updated body
-    const newNote = {
-      ...currentNotee,
-      body: text,
-    };
+    // const newNote = {
+    //   ...currentNotee,
+    //   body: text,
+    // };
 
     // Filter out the current note from the notes array
-    const filteredNotes = notes.filter((note) => note.id !== currentNote.id);
+    // const filteredNotes = notes.filter((note) => note.id !== currentNote.id);
 
     // Add the updated note back to the notes array
-    setNotes([...filteredNotes, newNote]);
+    // setNotes([...filteredNotes, newNote]);
 
-    // setNotes((oldNotes) => {
-    //   const newArr = [];
-    //   for (let i; i < oldNotes.length; i++) {
-    //     const oldNote = oldNotes[i];
-    //     if (oldNote.id === currentNoteId) {
-    //       return newArr.unshift({ ...oldNote, body: text });
-    //     } else {
-    //       return newArr.push(oldNote);
-    //     }
-    //   }
-    //   return newArr.push;
-    // });
+    setNotes((oldNotes) => {
+      const newArr = [];
+      for (let i = 0; i < oldNotes.length; i++) {
+        const oldNote = oldNotes[i];
+        if (oldNote.id === currentNoteId) {
+           newArr.unshift({ ...oldNote, body: text });
+        } else {
+           newArr.push(oldNote);
+        }
+      }
+      return newArr;
+    });
 
     //not rearranging notes
 
